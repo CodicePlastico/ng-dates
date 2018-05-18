366aff850cea64045de53de74ee0cd718cfd1d9e
Davide Brognoli <davide.brognoli@codiceplastico.com>
2018-05-18 12:03:17 +0200

    Date Picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'ng-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng';
+  public selectedDay: moment.Moment = moment().add('1', 'days');
+  public minDay: moment.Moment = moment().subtract('1', 'days');
+  public maxDay: moment.Moment = moment().add('2', 'months');
+
+  public show(string, evt) {
+    console.log(string, evt);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgDatesModule } from 'ng-dates';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgDatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
